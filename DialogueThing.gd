f1bcd1dcc32165f82fde1ee91903d11d2ab48724
LoparPanda <60553740+LoparPanda@users.noreply.github.com>
2021-10-01 00:35:50 -0700

    fghdfghgfdhdf

--- DialogueThing.gd
+++ DialogueThing.gd
@@ -7,6 +7,10 @@ extends Area
 
 export(String) var dialogue_scene: String
 
+export(int) var dlg_max: int = 0
+
+var counter := 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,12 +21,16 @@ func _process(delta):
 	pass
 
 func _unhandled_input(event):
+	#counter = max(dlg_min, min(counter, dlg_max))
 	if (
 		event.is_action_pressed("ui_select")
 		and not event.is_echo() and dialogue_scene):
 			for b in get_overlapping_bodies():
 				if b.is_in_group("player"):
-					GameDlg.present(dialogue_scene)
+					var use_dlg = "%s%s" % [dialogue_scene, (counter + 1) if dlg_max > 0 else ""]
+					GameDlg.present(use_dlg)
+	
+					if dlg_max > 0:
+						counter =  wrapi(counter + 1, 0, dlg_max)
 					get_tree().set_input_as_handled()
 					return
-	
